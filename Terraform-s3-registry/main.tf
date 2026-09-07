79e79d6285499160940813c74a90d854e3814188
Lakshya Borra <borralakshya7@gmail.com>
2026-09-07 23:39:41 +0530

    feat: configure s3 versioning and lifecycle rules natively in terraform

--- Terraform-s3-registry/main.tf
+++ Terraform-s3-registry/main.tf
@@ -22,3 +22,29 @@ resource "aws_s3_bucket" "ml_registry" {
         ManagedBy = "terraform"
     }
 }
+
+# 1. Enable object versioning on our terraform-mamnged bucket
+resource "aws_s3_bucket_versioning" "ml_registry_versioning" {
+    bucket = aws_s3_bucket.ml_registry.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
+# 2. configure the lifecycle policy to move old models to glacier archiving after 30 days
+resource "aws_s3_bucket_lifecycle_configuration" "ml_registry_lifecycle" {
+    bucket = aws_s3_bucket.ml_registry.id
+    rule {
+        id = "archive-old-model-versions"
+        status = "Enabled"
+
+        filter {
+            prefix = "models/"
+        }
+        # transition historical (non-current) versions to glacier
+        noncurrent_version_transition {
+            noncurrent_days = 30
+            storage_class = "GLACIER"
+        }
+    }
+}
